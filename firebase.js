afb5a3c77956ebf62ee22860c96a75d59155fe58
pclijten <pclijten@gmail.com>
2026-03-24 10:49:44 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js';
+import { getAuth }        from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js';
+import { getFirestore }   from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDkrkHrPW4pPu0_ZVKGma-AVF4-gTtk9R8",
-  authDomain: "niste-1113e.firebaseapp.com",
-  projectId: "niste-1113e",
-  storageBucket: "niste-1113e.firebasestorage.app",
+  apiKey:            "AIzaSyDkrkHrPW4pPu0_ZVKGma-AVF4-gTtk9R8",
+  authDomain:        "niste-1113e.firebaseapp.com",
+  projectId:         "niste-1113e",
+  storageBucket:     "niste-1113e.firebasestorage.app",
   messagingSenderId: "292138618317",
-  appId: "1:292138618317:web:50984a6bff31725d6a0884"
+  appId:             "1:292138618317:web:50984a6bff31725d6a0884",
 };
 
-const app = initializeApp(firebaseConfig);
-
+export const app  = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db   = getFirestore(app);
